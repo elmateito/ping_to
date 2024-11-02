bac394d6ea9001bf9eed71f0d1d58cda6211ceb6
elmateito <mateo.se.arias@gmail.com>
2024-11-01 22:05:17 -0500

    ip iter by range

--- ping_to.py
+++ ping_to.py
@@ -1,27 +1,31 @@
-from socket import gethostbyname, gethostname
+from socket import gethostbyname, gethostname, gethostbyaddr
 from os import system
 from json import dumps
 
 hostname = gethostname()
 ip_host = gethostbyname(hostname)
 
-def ping():
-    ping_to = ['192.168.0.28', '192.168.0.26', '192.168.0.136', '192.168.0.250']
-    availables = []
-
+def mk_txt(ip_host, hostname, availables, assigned):
     reg = open('ip_reg.txt', 'w')
-    title = dumps({'locals': {'ip': ip_host, 'host': hostname}}, indent=4)
-    reg.write(f'{title}\n\n')
-
-    for ip in ping_to:
-        status = system(f'ping -n 2 -w 1 {ip}')
-        if status == 0: print(f'{ip} NOT available')
-        else:
-            availables.append(ip)
-            print(f'{ip} AVAILABLE')
-    body = str(dumps({'availables': availables}, indent=4))
+    content = {'result': {'locals': {'ip': ip_host, 'host': hostname},
+                'availables': availables,
+                'assigned': assigned}}
+    body = dumps(content, indent=4)
     reg.write(body)
     reg.close()
     reg = open('ip_reg.txt', 'r')
+
+def ping():
+    availables = []
+    assigned = []
+    for i in range(1, 16):
+        try:
+            ip = f'192.168.0.{i}'
+            status = system(f'ping -n 2 -w 1 {ip}')
+            if status == 0: assigned.append({ip: gethostbyaddr(ip)[0]})
+            else: availables.append(ip)
+            mk_txt(ip_host, hostname, availables, assigned)
+        except: 
+            print('conn failed')
 
 ping()
