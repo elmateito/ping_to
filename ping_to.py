5aed3f0d260b935b7b35649444407ae6a4b604e1
elmateito <mateo.se.arias@gmail.com>
2024-11-03 23:01:25 -0500

    handling exceptions

--- ping_to.py
+++ ping_to.py
@@ -6,30 +6,31 @@ from time import time
 hostname = gethostname()
 ip_host = gethostbyname(hostname)
 
-def mk_txt(ip_host, hostname, availables, assigned):
+def mk_txt(ip_host, hostname, availables, assigned, failed):
     reg = open('ip_reg.txt', 'w')
     content = {'result': {'locals': {'ip': ip_host, 'host': hostname},
                 'availables': availables,
-                'assigned': assigned}}
+                'assigned': assigned,
+                'failed': failed}}
     body = dumps(content, indent=4)
     reg.write(body)
     reg.close()
     reg = open('ip_reg.txt', 'r')
+    return reg.read()
 
 def ping():
     timer = time()
-    availables = []
-    assigned = []
-    for i in range(0, 17):
+    availables, assigned, failed = [], [], []
+    for i in range(0, 17): #iter from 192.168.0.0 to 192.168.0.8
+        ip = f'192.168.0.{i}'
         try:
-            ip = f'192.168.0.{i}'
             status = system(f'ping -n 2 -w 1 {ip}')
             if status == 0: assigned.append({ip: gethostbyaddr(ip)[0]})
-            else: availables.append(ip)
-            mk_txt(ip_host, hostname, availables, assigned)
+            else: availables.append(ip)  
         except: 
-            print('\nconn failed\n')
-    file = open('ip_reg.txt', 'r')
-    print(f'{file.read()}\n--- execution time: %s secs ---\n' % (round((time() - timer), 3)))
+            failed.append(ip)
+            print(f'\n{ip}: ping failed')
+        file = mk_txt(ip_host, hostname, availables, assigned, failed) 
+    print(f'{file}\n--- execution time: %s secs ---\n' % (round((time() - timer), 3)))
 
 ping()
